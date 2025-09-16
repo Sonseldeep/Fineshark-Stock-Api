0062dbe74512def38bf984b5b0a9208d245b90dd
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-09-16 22:56:42 +0545

    Add sorting capabilities to Stock query with SortBy and IsDescending properties

--- Stock.API/Helper/QueryObject.cs
+++ Stock.API/Helper/QueryObject.cs
@@ -2,6 +2,11 @@ namespace Stock.API.Helper;
 
 public class QueryObject
 {
+    // for searching or filtering 
     public string? Symbol { get; set; } = null!;
     public string? CompanyName { get; set; } = null!;
+    
+    // for sorting 
+    public string? SortBy { get; set; } = null!;
+    public bool IsDescending { get; set; } = false;
 }
