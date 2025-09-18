2166a9d24b895fe551d538b6321f41a0c4fd5ee8
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-09-18 15:25:08 +0545

    Add JWT authentication support with configuration in appsettings.json

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Stock.API.Data;
 using Stock.API.Models;
 using Stock.API.Repository;
@@ -28,11 +30,35 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
         options.Password.RequireDigit = true;
         options.Password.RequireLowercase = true;
         options.Password.RequireUppercase = true;
-        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireNonAlphanumeric = true;
         options.Password.RequiredLength = 8;
 
     })
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme =
+            options.DefaultChallengeScheme =
+                options.DefaultForbidScheme =
+                    options.DefaultScheme =
+                        options.DefaultSignInScheme =
+                            options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    })
+    .AddJwtBearer(options =>
+        {
+           options.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidateIssuerSigningKey = true,
+               ValidIssuer = builder.Configuration["JWT:Issuer"],
+               ValidAudience = builder.Configuration["JWT:Audience"],
+               IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"]!))
+           };
+        }
+        );
+
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
@@ -46,6 +72,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
