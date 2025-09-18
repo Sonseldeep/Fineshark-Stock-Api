5e3b322331b12ce2d1a5daac19b9298ec8c8a0e0
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-09-18 20:50:08 +0545

    Add login functionality to AccountController with validation and token generation

--- Stock.API/Controllers/AccountController.cs
+++ Stock.API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Stock.API.Dtos.Account.Request;
 using Stock.API.Dtos.Account.Response;
 using Stock.API.Models;
@@ -12,11 +13,47 @@ public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly ITokenService _tokenService;
+    private readonly SignInManager<AppUser> _signInManager;
 
-    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService,SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
         _tokenService = tokenService;
+        _signInManager = signInManager;
+      
+    }
+
+    [HttpPost("api/accounts/login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(new { errors = ModelState.Values.SelectMany(v => v.Errors) });
+        }
+
+        var user = await _userManager.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+        
+        if (user is null)
+        {
+            return Unauthorized(new { error = "Invalid username" });
+        }
+        
+        var result = await _signInManager.CheckPasswordSignInAsync(user,loginDto.Password, false);
+
+        if (!result.Succeeded)
+        {
+            return Unauthorized( "Invalid username or password" );
+        }
+        
+        var newUser = new NewUserDto
+        {
+            UserName = user.UserName ?? string.Empty,
+            Email = user.Email ?? string.Empty,
+            Token = _tokenService.CreateToken(user)
+        };
+
+        return Ok(newUser);
+
     }
 
     [HttpPost("api/accounts/register")]
